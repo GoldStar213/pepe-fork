dea86381a45dad87cdfcbfcdf064522a5e685cd6
GoldStar213 <seniordev213@gmail.com>
2023-11-06 11:26:19 +0900

    fixed some responsive5

--- js/index.js
+++ js/index.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $("#mobile-menu-button").click(function() {
         $("#mobile-menu").slideToggle();
     });
+
+    $("#mobile-menu a").on('click', function() {
+        $("#mobile-menu").slideToggle();
+    });
 });
